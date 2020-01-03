add3ceb9a9fcb40f5e98379613b1db394b88896f
Jonathan Monroy <wackson86.jm@gmail.com>
2020-01-03 14:34:18 -0300

    Galerias de imagenes lista

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,4 @@
 window.onscroll = () => {
     const nav = document.querySelector('#navbar');
-    console.log(scrollY);
-    if(this.scrollY <= 20) nav.className = 'navbar navbar-expand-lg navbar-light bg-light'; else nav.className = 'scroll navbar navbar-expand-lg navbar-light bg-light';
+    if(this.scrollY <= 20) nav.className = 'navbar navbar-expand-lg navbar-light bg-transparent'; else nav.className = 'scroll navbar navbar-expand-lg navbar-light';
   };
